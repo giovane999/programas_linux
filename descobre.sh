26eb87bb55fd95ed54e511cdd4189fa85413fead
giovane999 <38047844+giovane999@users.noreply.github.com>
2018-10-18 00:30:58 -0300

    Add files via upload

---
+++ descobre.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+if [ $1 == "" ]
+then
+	echo " "
+        echo "------------------------------------------"
+        echo "-                                        -"
+        echo "---- Desenvolvido por Eric Rodrigues  ----"
+	echo "---- Modo de uso : http://alvo.com.br ----"
+        echo "----    Ericrodrigo999@gmail.com      ----"
+	echo "-                                        -"
+        echo "------------------------------------------"
+        echo " "
+else
+	echo " "
+	echo "Escaniando Aguarde..."
+	echo " "
+
+	#Diretorio#
+	for palavra in $(cat /usr/share/dirb/wordlists/common.txt)
+
+	do
+		resposta1=$(curl -s -o /dev/null -w "%{http_code}" $1/$palavra/)
+		if [ $resposta1 == "200" ]
+			then
+			echo "Diretorio encontrado -> $1/$palavra"
+		fi
+        done
+
+        echo " "
+        echo "------------------------------------------"
+        echo " "
+        echo "       Você rodou toda a WordList         "
+        echo "        Ericrodrigo999@gmail.com          "
+        echo " "
+        echo "------------------------------------------"
+        echo " "
+
+fi
