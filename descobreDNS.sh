1f0f17048b3e646c52e5359f4a12a28a42ed6b7a
Eric Rodrigues <38047844+giovane999@users.noreply.github.com>
2018-12-26 15:57:53 -0200

    Create descobreDNS.sh

---
+++ descobreDNS.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+if [ $1 == "" ]
+then
+        echo " "
+        echo "------------------------------------------"
+        echo "-                                        -"
+        echo "---- Desenvolvido por Eric Rodrigues  ----"
+        echo "---- Modo de uso :        alvo.com.br ----"
+        echo "----    Ericrodrigo999@gmail.com      ----"
+        echo "-                                        -"
+        echo "------------------------------------------"
+        echo " "
+else
+        echo " "
+        echo "Escaniando Aguarde..."
+        echo " "
+
+        #Diretorio#
+        #for palavra in $(cat /usr/share/dirb/wordlists/common.txt)
+        for palavra in $(cat rato.txt)
+
+
+        do
+                #echo "$1/$palavra"
+                resposta1=$(curl -s -o /dev/null -w "%{http_code}" $palavra.$1)
+                if [ $resposta1 == "200" ]
+                        then
+                        echo "Diretorio encontrado -> http://$palavra.$1"
+                fi
+        done
+
+        echo " "
+        echo "------------------------------------------"
+        echo " "
+        echo "       Você rodou toda a WordList         "
+        echo "        Ericrodrigo999@gmail.com          "
+        echo " "
+        echo "------------------------------------------"
+        echo " "
+
+fi
