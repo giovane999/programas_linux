b0cbd7675e5c276d870fc15ea3bc59146374096e
Eric Rodrigues <38047844+giovane999@users.noreply.github.com>
2019-02-01 14:38:59 -0200

    Update parsinurl.sh

--- parsinurl.sh
+++ parsinurl.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
-if ["$1" == ""]
+#! /bin/bash
 
+if [ -z $1 ]
 then
-	echo "Programa busca links e guarda em uma lista, em seguida faz um loop com o comando host para pegar o ip do servidor"
-	echo "ParsingUrl - Eric"
-	echo "Exemplo de uso $1 10.0.0.1"
+	echo " "
+        echo "============================================================"
+        echo "Desenvolvido por Eric Rodrigues"
+	echo "Modo de uso : ./descobre http://alvo.com.br "
+        echo "Ericrodrigo999@gmail.com"
+        echo "============================================================"
+        echo " "
 else
 
 wget $1
 
-cat index.html | grep href | cut -d "/" -f3 | grep "\." | sort -u | cut -d '"' -f1 > listaUrl
+cat $1.html | grep href | cut -d "/" -f3 | grep "\." | sort -u | cut -d '"' -f1 > listaUrl
 rm index.html
 
 for url in $(cat listaUrl);do host $url;done | grep "has address"
