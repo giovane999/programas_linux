32eafc830d06660d4b359ded6e17f549f8c667ae
Eric Rodrigues <38047844+giovane999@users.noreply.github.com>
2019-02-01 14:45:54 -0200

    Update parsinurl.sh

--- parsinurl.sh
+++ parsinurl.sh
@@ -13,7 +13,7 @@ else
 
 wget $1
 
-cat parsinurl.html | grep href | cut -d "/" -f3 | grep "\." | sort -u | cut -d '"' -f1 > listaUrl
+cat index.html | grep href | cut -d "/" -f3 | grep "\." | sort -u | cut -d '"' -f1 > listaUrl
 rm index.html
 
 for url in $(cat listaUrl);do host $url;done | grep "has address"
