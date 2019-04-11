6b2334e0afbcde32418f8f9dd478dd9ea3e28614
Eric Rodrigues <38047844+giovane999@users.noreply.github.com>
2019-04-11 13:40:01 -0300

    Update portcan.py

--- portcan.py
+++ portcan.py
@@ -1,23 +1,25 @@
+#!/usr/bin/dev python 
+
 import socket
 
 try:
 
-	ip = raw_input('Digite o alvo: ')
-	begin_port = input('Digite a porta de inicio: ')
-	end_port = input('Digite a porta de final: ')
-	print('Escaniando aguarde...')
-	for i in range(begin_port,end_port+1):
+    ip = input("Digite o ip do Alvo: ")
+    portaInicial = int(input("Porta inicial: "))
+    portaFinal = int(input("Porta final: "))
+    print("Escaniando...")
 
-		sock = socket.socket()
-		sock.settimeout(0.5)
-		result = sock.connect_ex((str(ip),int(i)))
-		
-		if result == 0:
-			print 'Port ' + str(i) + " is OPEN on " + str(ip)
-			sock.close()
-		else:
-#			print 'Port ' + str(i) + " is CLOSE on " + str(ip) 
-			sock.close()
+    for i in range(portaInicial,portaFinal+1):
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        resultado = sock.connect_ex((str(ip), int(i)))
+
+        if resultado == 0: 
+            print("Porta {} Aberta {}".format(i,ip))
+            sock.close()
+        else:
+            sock.close()
+
 except KeyboardInterrupt:
-	print "exit ..."
-	exit()
+    print("Fim")
+    exit()
